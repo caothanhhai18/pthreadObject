848ef3bfcd63cd7c2e0997b1f2f8235e794217cd
caothanhhai18 <caothanhhai18@gmail.com>
2017-12-14 23:45:17 +0700

    dede

--- MyThread.cpp
+++ MyThread.cpp
@@ -1,5 +1,5 @@
 #include "MyThread.h"
-
+if(){
 MyThread::MyThread(void *(*fn_ptr)(void *), void *arg) : m_fn_ptr(fn_ptr), m_arg(arg)
 {
 }
@@ -30,3 +30,4 @@ bool MyThread::Run()
     return pthread_create(&m_pThreadPtr,NULL,m_fn_ptr,m_arg);
 }
 
+}
